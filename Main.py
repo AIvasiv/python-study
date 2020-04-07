66392cdde6ed0fbcc6dadd2cebbe6d0095cde1f5
aivasiv <ivasiv.andriy@gmail.com>
2020-04-07 21:06:53 +0300

    init

---
+++ Main.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+matrix = [[j+1 for j in range(5)] for i in range(5)]
+print(matrix)
+
+new_matrix = []
+for itr in matrix:
+    for itr1 in itr:
+        new_matrix.append(itr1)
+print(new_matrix)
+
+new_matrix2 = [val for itr in matrix for val in itr ]
+print(new_matrix2)
+
