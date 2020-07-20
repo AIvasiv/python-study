99ff1230486169f01ca123e1637a22208380bb87
aivasiv <ivasiv.andriy@gmail.com>
2020-07-20 17:54:32 +0300

    added function to calculate list average

--- basics.py
+++ basics.py
@@ -1,1 +1,16 @@
-                                            
+import datetime;
+
+#print(dir(int))
+#help(int.real)
+
+my_list = [1.1, 1.2, 1.3]
+#print(dir(list))
+
+def calculateListAverage(list):
+    average = 0.0
+    for i in list:
+        average += i
+    average = average / len(list)
+    print (average)
+    
+calculateListAverage(my_list)
