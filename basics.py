8e70fd1a267dbb1b5c82e8ad1b51a1e3e5bf5707
aivasiv <ivasiv.andriy@gmail.com>
2020-08-03 20:44:56 +0300

    small piece to edit user inputs

--- basics.py
+++ basics.py
@@ -3,6 +3,8 @@ import datetime;
 #print(dir(int))
 #help(int.real)
 
+
+
 my_list = [1.1, 1.2, 1.3]
 #print(dir(list))
 
@@ -14,3 +16,24 @@ def calculateListAverage(list):
     print (average)
     
 calculateListAverage(my_list)
+
+#testing the strings
+user_input = input("Please, enter your name: ")
+message1 = f"Hello {user_input}"
+message2 = "Hello %s" % (user_input)
+print(message1)
+
+
+#*****************************
+lis_of_all_user_inputs = []
+
+while(user_input != '/end'):
+    temp_string = input('Please, enter the message: ').capitalize()
+    user_input = temp_string
+    if(temp_string[len(temp_string)-1] != '.' or temp_string[len(temp_string)-1] != '?' or temp_string[len(temp_string)-1] != '!'):
+        temp_string += '.'
+    lis_of_all_user_inputs.append(temp_string)
+print (lis_of_all_user_inputs)
+
+
+
